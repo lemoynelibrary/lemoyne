9c8063876540297b5fb156e3d78b44aa102eca5f
Tom Keays <tomkeays@gmail.com>
2020-04-09 09:58:50 -0400

    take snapshot of staging server

---
+++ catalog/screens/staging-snaphot/banner-loader.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $('ul.sf-menu').superfish({
+    hoverClass: 'sfHover',
+    delay: 750,
+    animation: {opacity:'show',height:'show'},
+    speed: 'fast',
+    autoArrows: false,
+    dropShadows: false
+  });
+});
