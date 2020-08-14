944427ee02a7992a5850ba9f61ba2e9aa5ada2d9
Tom Keays <tomkeays@gmail.com>
2020-08-14 12:21:51 -0400

    wordpress : convert tabs to spaces

--- wordpress/lemoynelibrary/header.php
+++ wordpress/lemoynelibrary/header.php
@@ -10,27 +10,27 @@
 <!--[if IE 7]>     <html class="no-js lt-ie9 lt-ie8" <?php language_attributes(); ?>> <![endif]-->
 <!--[if IE 8]>     <html class="no-js lt-ie9" <?php language_attributes(); ?>> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
-	<head>
-		<meta charset="<?php bloginfo('charset'); ?>">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width">
+  <head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width">
 
-		<link rel="profile" href="http://gmpg.org/xfn/11">
-		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
-		
-		<!--wordpress head-->
-		<?php wp_head(); ?>
-	</head>
-	<body <?php body_class(); ?>>
-		<!--[if lt IE 8]>
-			<p class="ancient-browser-alert">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/" target="_blank">upgrade your browser</a>.</p>
-		<![endif]-->
-		
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+    
+    <!--wordpress head-->
+    <?php wp_head(); ?>
+  </head>
+  <body <?php body_class(); ?>>
+    <!--[if lt IE 8]>
+      <p class="ancient-browser-alert">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/" target="_blank">upgrade your browser</a>.</p>
+    <![endif]-->
+    
 
 <?php include_once("header-include.txt") ?>
 
 
-	<nav id=”s-lib-bc” aria-label=”breadcrumb”>  
+  <nav id=”s-lib-bc” aria-label=”breadcrumb”>  
    <ol id="s-lib-bc-list" class="breadcrumb">
       <li>
         <a title="Noreen Reale Falcone Library" href="http://resources.library.lemoyne.edu/library">Noreen Reale Falcone Library</a>
@@ -44,30 +44,30 @@
       <li>
         <a title="Featured" href="<?php echo esc_url(home_url('/')); ?>category/featured/">Featured</a>
       </li>
-		</ol>
-	</nav>
-		
-		
-		
-		<div class="container page-container">
-			<?php do_action('before'); ?> 
-			<header role="banner">
-				<div class="row row-with-vspace site-branding">
+    </ol>
+  </nav>
+    
+    
+    
+    <div class="container page-container">
+      <?php do_action('before'); ?> 
+      <header role="banner">
+        <div class="row row-with-vspace site-branding">
 
-					<div class="col-md-12 page-header-top-right">
-						<div class="sr-only">
-							<a href="#content" title="<?php esc_attr_e('Skip to content', 'bootstrap-basic'); ?>"><?php _e('Skip to content', 'bootstrap-basic'); ?></a>
-						</div>
-						<?php if (is_active_sidebar('header-right')) { ?> 
-						<div class="pull-right">
-							<?php dynamic_sidebar('header-right'); ?> 
-						</div>
-						<div class="clearfix"></div>
-						<?php } // endif; ?> 
-					</div>
-				</div><!--.site-branding-->
-				
-			</header>
-			
-			
-			<div id="content" class="row row-with-vspace site-content">
+          <div class="col-md-12 page-header-top-right">
+            <div class="sr-only">
+              <a href="#content" title="<?php esc_attr_e('Skip to content', 'bootstrap-basic'); ?>"><?php _e('Skip to content', 'bootstrap-basic'); ?></a>
+            </div>
+            <?php if (is_active_sidebar('header-right')) { ?> 
+            <div class="pull-right">
+              <?php dynamic_sidebar('header-right'); ?> 
+            </div>
+            <div class="clearfix"></div>
+            <?php } // endif; ?> 
+          </div>
+        </div><!--.site-branding-->
+        
+      </header>
+      
+      
+      <div id="content" class="row row-with-vspace site-content">
